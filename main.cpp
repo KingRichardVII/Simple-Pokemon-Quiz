80f1bc7972988eab4de2d0582f64b4273f088558
Richard Phan <richard.phan626@gmail.com>
2025-09-25 23:40:32 -0700

    Add files via upload
    brieflyu started planning program. Possible variables, user input and output, if statements, functions and classes

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//function prototypes
+//create a class called Question that includes variables including the question and answers
+
+
+int main() {
+	//Beginning message for quiz. Make aesthetic.
+	cout << "Pokemon Quiz";
+
+	//Ask for name
+
+	//ask for age
+
+	//ask if they want to take the quiz
+		//if statement
+
+	//create instances of questions (5-10 questions?)
+
+	//set questions. MCQ options, correct option, number of pts
+
+	//call member function ask question
+
+	//allow user to enter option (1-4) or (A-D)
+		//touppper
+		//validate input
+
+	//show user total score
+		//show some kind of message
+		// "EXCELLENT"
+		// "Not bad"
+		//"Ugh.... that couldve went better"
+
+
+
+
+
+	return 0;
+}
+
+//functions
